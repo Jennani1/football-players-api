11fb661cf01f7b29fcb92a187033071fe830e9b5
Jennani1 <Jennani2@icloud.com>
2026-09-14 11:25:37 +0200

    test: require GET /api/players to return players

--- tests/players.test.js
+++ tests/players.test.js
@@ -2,9 +2,11 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('GET /api/players', () => {
-  it('should return status 200', async () => {
+  it('should return a list of players', async () => {
     const response = await request(app).get('/api/players');
 
     expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body.length).toBeGreaterThan(0);
   });
 });
